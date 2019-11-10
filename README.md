29e866745c5810a24f195a5198cfedb561a78029
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-11-10 17:00:06 +0100

    canvas pb

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Canvas-Training
+
+[Here is the link to the page]().
+
+## Goal ##
+
+The point of this project is to train myself to use Canvas.
+
+<!-- ## Dates ##
+
+This site has been made during my internship at Becode on x the xth of November. -->
---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// console.log("test")
+
+window.onload = () => {
+
+// le pendu
+let canvasPendu = document.getElementById("pendu");
+let ctxPendu = canvasPendu.getContext("2d");
+
+//bottom case
+
+ctxPendu.beginPath();
+ctxPendu.moveTo( 0,0);
+ctxPendu.lineTo( 0, 400);
+ctxPendu.lineTo( 100, 400);
+ctxPendu.lineTo( 100, 0);
+ctxPendu.lineTo( 100, 0);
+ctxPendu.closePath();
+ctxPendu.strokeStyle = "black";
+ctxPendu.stroke();
+// ctxPendu.fillStyle = "black";
+// ctxPendu.fill();
+
+
+
+var c = document.getElementById('mon_canvas');
+var ctx = c.getContext("2d");
+
+// Voile du bateau
+ctx.beginPath();      // Début du chemin
+ctx.moveTo(150,80);   // Le tracé part du point 150,80
+ctx.lineTo(300,230);  // Un segment est ajouté vers 300,230
+ctx.lineTo(150,230);  // Un segment est ajouté vers 150,230
+ctx.closePath();      // Fermeture du chemin
+ctx.fillStyle = "lightblue"; // Définition de la couleur de remplissage
+ctx.fill();           // Remplissage du dernier chemin tracé
+
+// Coque du bâteau
+ctx.beginPath();      // Début d'un autre chemin
+ctx.moveTo(50,250);
+ctx.lineTo(100,300);
+ctx.lineTo(250,300);
+ctx.lineTo(300,250);
+ctx.fillStyle = "peru";
+ctx.strokeStyle = "sienna"; // Définition de la couleur de contour
+ctx.lineWidth = 5;         // Définition de la largeur de ligne
+ctx.fill();            // Application du remplissage
+ctx.stroke();          // Application du contour
+
+// Mât
+ctx.beginPath();
+ctx.moveTo(140,50);
+ctx.lineTo(140,250);
+ctx.lineWidth = 10;
+ctx.stroke();
+
+}
